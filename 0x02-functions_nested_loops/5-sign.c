090139ecf1acf3ea138285aef6936858d310f578
Zahraa1379 <sityrant0@gmail.com>
2023-06-20 10:18:14 -0700

    Sign task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n : the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+		return (1);
+	else if (n == 0)
+		return (0);
+	else if
+		return (-1);
+}

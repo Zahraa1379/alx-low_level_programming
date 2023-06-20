3b618d5b9e4b4a60f767c08d9eeaeffc4152fbe3
Zahraa1379 <sityrant0@gmail.com>
2023-06-20 04:37:22 -0700

    the alphabet game task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet  - prints the alphabet, in lowercase, followed by a new line
+ * return : 0
+ */
+void print_alphabet(void)
+{
+		for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0);
+}

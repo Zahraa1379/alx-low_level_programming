ffdc3a682ac4ff9a48412173e32cddb2e75bb0c4
Zahraa1379 <sityrant0@gmail.com>
2023-06-26 06:09:40 -0700

    A good engineer thinks in reverse task

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -5,16 +5,24 @@
  */
 void rev_string(char *s)
 {
-int length = strlen(s);
-int start = 0;
-int end = length - 1;
+	int count = 0, i, j;
+	char *str, temp;
 
-while (start < end)
-{
-char temp = s[start];
-s[start] = s[end];
-s[end] = temp;
-start++;
-end--;
-}
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+	str = s;
+
+	for (i = 0; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
+	}
 }
